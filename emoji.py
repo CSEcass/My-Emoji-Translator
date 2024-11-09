9a40be8992e82d4efdb71096c2f6baa8393988b9
Cassidy <cassidy.reyes@csedge.org>
2024-11-09 12:00:29 -0800

    first save

--- emoji.py
+++ emoji.py
@@ -1,2 +1,18 @@
 print('______Beginning of Code______')
 
+# OBJECTIVE: retrieve words & map words to emoji
+#0 - User Input Stored
+Inp = input()
+UserInp = Inp.split
+#1 - Make dictionary for words
+EmojiDict = { 
+    'lol' : '😂', 
+    'what' : '🤨',
+    'high' : '⬆️',
+    'no' : '🔴',
+    'yes' : '🟢'
+}
+print(EmojiDict['high'])
+#2 - Replace words with emojis
+for UserInp in EmojiDict.items():
+    print()
